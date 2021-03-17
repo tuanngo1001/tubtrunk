f0d14a9e8b331d80e82b941c7073c6dbe3501be5
tuanngo1001 <tuan.ngo1001@gmail.com>
2021-03-17 14:55:14 -0500

    Add Authentication controller

---
+++ source/tubtrunk/lib/Controllers/authenticationController.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import "../Views/mainView.dart";
+import 'package:tubtrunk/Utils/globalSettings.dart';
+
+class AuthenticationController {
+  String email = "";
+  String password = "";
+
+  void handleEmail(String input) {
+    email = input;
+  }
+
+  void handlePassword(String input) {
+    password = input;
+  }
+
+  void login() async {
+    var map = new Map<String, String>();
+    map['UserEmail'] = email;
+    map['UserPassword'] = password;
+
+    await http
+        .post(GlobalSettings.serverAddress + "loginUser.php", body: map)
+        .then((response) => {
+              if (response.statusCode == 200)
+                {
+                  print("loginUser: " + response.body)
+                }
+            });
+  }
+
+  Future<dynamic> signup(String email, String password) async {
+    try {
+      var res = await http.post('', body: {
+        'email': email,
+        'password': password,
+      });
+      return res?.body;
+    } finally {}
+  }
+}
