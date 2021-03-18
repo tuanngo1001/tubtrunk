f46b2637d00ac4d630ad9aafad1d3826115b6366
tuanngo1001 <tuan.ngo1001@gmail.com>
2021-03-17 23:57:06 -0500

    Add global var to store session

--- source/tubtrunk/lib/Utils/globalSettings.dart
+++ source/tubtrunk/lib/Utils/globalSettings.dart
@@ -4,4 +4,7 @@ class GlobalSettings {
   static String serverAddress = "https://tubtrunk.tk/";
   static DateFormat dateFormatted = DateFormat("MM/dd/yyyy");
   static DateFormat timeFormatted = DateFormat("HH:mm");
+  static String email = "";
+  static String token = "";
+  static String username = "";
 }
