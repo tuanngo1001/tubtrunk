fa5729d144787e64fc925a71ee5205141b9a958e
Quoc Nguyen <charlesivnguyen4@yahoo.com>
2021-03-17 20:45:20 -0500

    add new script that check for user existance, and update updateUserName.php

--- server_scripts/updateUserName.php
+++ server_scripts/updateUserName.php
@@ -1,14 +1,19 @@
 <?php
 require_once('connectDB.php');
 include('connectDB.php');
+include('checkUserExist.php');
 
 $con = $GLOBALS['con'];
 
 $userEmail = $_POST['UserEmail'];
 $userName = $_POST['UserName'];
 
-$updateQuery = "UPDATE User SET uUserName = '$userName' WHERE uEmail = '$userEmail'";
-$result = mysqli_query($con, $updateQuery);
-
-die($result);
+if (checkUser($userEmail))
+{
+    $updateQuery = "UPDATE User SET uUserName = '$userName' WHERE uEmail = '$userEmail'";
+    $result = mysqli_query($con, $updateQuery);
+    
+    die($result);
+}
+die("Not found");
 ?>
