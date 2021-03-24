3810b29c9e74e6f110ae7980ec06dbececaaf68b
Peter <ducanh199818@gmail.com>
2021-03-23 20:44:15 -0500

    Delete unnecessary unit tests

--- source/tubtrunk/test/controller_tests/leaderboard_controller_test.dart
+++ source/tubtrunk/test/controller_tests/leaderboard_controller_test.dart
@@ -4,18 +4,6 @@ import 'package:tubtrunk/Models/userModel.dart';
 
 void main() {
   group('leaderboardController Tests', () {
-    group('Initialization Test', () {
-      test("The leaderboardController instance should be not null", () {
-        LeaderboardController testLeaderboardController = new LeaderboardController();
-        expect(testLeaderboardController, isNotNull);
-      });
-
-      test("usersList variable shouldn't be null", () {
-        LeaderboardController testLeaderboardController = new LeaderboardController();
-        expect(testLeaderboardController.usersList, isNotNull);
-      });
-    });
-
     group('leaderboardController Functions Test', () {
       test("fetchAllUsers should return a List<UserModel>", () {
         LeaderboardController testLeaderboardController = new LeaderboardController();
