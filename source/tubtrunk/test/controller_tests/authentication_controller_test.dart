60d327db5744ed90016049d8fa442fe364b346cc
tuanngo1001 <tuan.ngo1001@gmail.com>
2021-03-23 22:35:36 -0500

    Create Test for authen Controller

---
+++ source/tubtrunk/test/controller_tests/authentication_controller_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tubtrunk/Controllers/authenticationController.dart';
+
+void main() {
+  group('leaderboardController Tests', () {
+    group('leaderboardController Functions Test', () {
+      //
+    });
+  });
+}
