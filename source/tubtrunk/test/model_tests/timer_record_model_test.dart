9bc21d810bf1201bed2f7258fc90ada078bb22b2
Trung Nguyen <narccoins@gmail.com>
2021-03-25 09:17:07 -0500

    add unit test for timerRecordModel

---
+++ source/tubtrunk/test/model_tests/timer_record_model_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tubtrunk/Models/timerRecordModel.dart';
+
+void main() {
+  group('timerRecordModel initialization test', () {
+    test('Testing instantiation with default contructor', () {
+      TimerRecordModel model = TimerRecordModel(date: 'Thursday', time: '10pm', duration: 1000, completed: 1, tag: 'Test tag');
+      expect(model.date, 'Thursday');
+      expect(model.time, '10pm');
+      expect(model.duration, 1000);
+      expect(model.completed, 1);
+      expect(model.tag, 'Test tag');
+    });
+
+    test('Testing instantiation with JSON constructor', () {
+      var json = new Map<String, dynamic>();
+      json['Date'] = 'Thursday';
+      json['Time'] = '10pm';
+      json['Duration'] = '1000';
+      json['Completed'] = '1';
+      json['Tag'] = 'Test tag';
+      TimerRecordModel model = TimerRecordModel.fromJson(json);
+      expect(model.date, 'Thursday');
+      expect(model.time, '10pm');
+      expect(model.duration, 1000);
+      expect(model.completed, 1);
+      expect(model.tag, 'Test tag');
+    });
+
+    test('Testing isCompleted()', () {
+      // successful record
+      TimerRecordModel model = TimerRecordModel(date: 'Thursday', time: '10pm', duration: 1000, completed: 1, tag: 'Test tag');
+      expect(model.isCompleted(), "Success!");
+      // failing record
+      model = TimerRecordModel(date: 'Thursday', time: '10pm', duration: 1000, completed: 0, tag: 'Test tag');
+      expect(model.isCompleted(), "Fail!");
+    });
+  });
+}
