51efae062157bddee53fc01ccfa5fbb53c209037
Trung Nguyen <narccoins@gmail.com>
2021-03-24 21:37:02 -0500

    add widget test for timer view

--- source/tubtrunk/test/controller_tests/timer_controller_test.dart
+++ source/tubtrunk/test/controller_tests/timer_controller_test.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:http/http.dart' as http;
--- source/tubtrunk/test/view_tests/timer_view_test.dart
+++ source/tubtrunk/test/view_tests/timer_view_test.dart
@@ -2,17 +2,34 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tubtrunk/Views/timerView.dart';
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 
 void main() {
     testWidgets('TimerView widget tests', (WidgetTester tester) async {
-      // create the widget
-      await tester.pumpWidget(new MediaQuery(
-        data: new MediaQueryData(),
-        child: new MaterialApp(home: new TimerView()),
-      ));
+      await tester.runAsync(() async {
+        // create the widget
+        Widget widget = new MediaQuery(
+          data: new MediaQueryData(),
+          child: new MaterialApp(home: new TimerView()),
+        );
+        await tester.pumpWidget(widget);
 
-      await tester.tap(find.text('Start'));
-      await tester.pump();
-      expect(find.text('Stop'), findsOneWidget);
+        // press start, countdown should start and the duration should not be zero
+        await tester.tap(find.text('Start'));
+        await tester.pump();
+        var finder = find.byType(CircularCountDownTimer);
+        var circularCountDownTimer = finder.evaluate().single.widget as CircularCountDownTimer;
+        expect(circularCountDownTimer.duration, isNot(0));
+
+        // stop the timer for the next test
+        await tester.tap(find.text('Stop'));
+        await tester.pump();
+
+        // press start and then stop, the countdown should stop with the remaining duration not changing
+        await tester.tap(find.text('Start'));
+        await tester.pump();
+        await tester.tap(find.text('Stop'));
+        await tester.pump(); // wait to make sure the timer has stopped
+      });
     });
 }
