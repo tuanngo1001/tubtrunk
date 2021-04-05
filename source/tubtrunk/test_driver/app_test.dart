81e1041feaa15c7cd700d0e9e05b66f1dcc50128
Quoc Nguyen <charlesivnguyen4@yahoo.com>
2021-04-04 22:58:28 -0500

    Remove unused import and space

--- source/tubtrunk/test_driver/app_test.dart
+++ source/tubtrunk/test_driver/app_test.dart
@@ -4,6 +4,7 @@ import 'package:test/test.dart';
 import 'reward_system_test.dart' as RewardSystemTest;
 import 'leaderboard_test.dart' as LeaderboardTest;
 import 'statistic_test.dart' as StatisticsTest;
+
 void main() {
   RewardSystemTest.runTests();
   StatisticsTest.runTests();
