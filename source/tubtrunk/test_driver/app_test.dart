8503ad4a19872cea71b1af140aca6bf9f6f6bec3
Trung Nguyen <narccoins@gmail.com>
2021-04-06 01:03:32 -0500

    add driver tests for timer view

--- source/tubtrunk/test_driver/app_test.dart
+++ source/tubtrunk/test_driver/app_test.dart
@@ -4,9 +4,11 @@ import 'package:test/test.dart';
 import 'reward_system_test.dart' as RewardSystemTest;
 import 'leaderboard_test.dart' as LeaderboardTest;
 import 'statistic_test.dart' as StatisticsTest;
+import 'timer_test.dart' as TimerTest;
 
 void main() {
   RewardSystemTest.runTests();
   StatisticsTest.runTests();
   LeaderboardTest.runTests();
+  TimerTest.runTests();
 }
